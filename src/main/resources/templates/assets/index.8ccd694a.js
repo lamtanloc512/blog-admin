b1e4577d10d737d21b40afc4b1e3a081694f202b
DELLPRECISION-L\LOCLT9 <lamtanloc512@gmail.com>
2022-10-09 21:59:03 +0700

    create h2db

--- src/main/resources/templates/assets/index.8ccd694a.js
+++
@@ -1,2 +0,0 @@
-import{j as e,c as t,R as r}from"./jsx-runtime.de477f7f.js";function n(){return e("div",{children:e("h1",{children:"Index Page"})})}t.createRoot(document.getElementById("root")).render(e(r.StrictMode,{children:e(n,{})}));
-//# sourceMappingURL=index.8ccd694a.js.map
--- src/main/resources/templates/assets/post.5a374052.js
+++
@@ -1,2 +0,0 @@
-import{j as e,c as t,R as o}from"./jsx-runtime.de477f7f.js";function r(){return e("div",{})}t.createRoot(document.getElementById("root")).render(e(o.StrictMode,{children:e(r,{})}));
-//# sourceMappingURL=post.5a374052.js.map
